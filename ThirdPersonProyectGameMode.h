ab4abe156880662c7d0be82972a0763eb0929834
Pedro Jesus <pedrojas89@gmail.com>
2015-07-15 02:54:32 +0200

    Create ThirdPersonProyectGameMode.h

---
+++ ThirdPersonProyectGameMode.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "ThirdPersonProyectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AThirdPersonProyectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AThirdPersonProyectGameMode(const FObjectInitializer& ObjectInitializer);
+};
+
+
+
